2544fe3620313c9c8fb10e69fc905e43cbd2c8f3
stylishgazelle <saniceggnog@gmail.com>
2017-05-10 03:19:17 -0400

    Add hand id to IRC commands

--- modules/gazelle/blackjack.py
+++ modules/gazelle/blackjack.py
@@ -4,18 +4,39 @@ import casino
 
 def hit(phenny, input):
     if casino.game:
-        casino.game.hit(input.uid)
+        handid = input.group(2)
+        if not handid:
+            casino.game.hit(input.uid)
+
+        if handid.isdigit():
+            casino.game.hit(input.uid, handid)
+        else:
+            phenny.write(('NOTICE', input.nick + " Specify the hand using a digit. Ex. !hit 1"))
 hit.commands = ['hit', 'Hit', 'h']
 hit.priority = 'high'
 
 def stand(phenny, input):
     if casino.game:
-        casino.game.stand(input.uid)
+        handid = input.group(2)
+        if not handid:
+            casino.game.stand(input.uid)
+
+        if handid.isdigit():
+            casino.game.stand(input.uid, handid)
+        else:
+            phenny.write(('NOTICE', input.nick + " Specify the hand using a digit. Ex. !stand 1"))
 stand.commands = ['stand', 'Stand', 'stay', 'Stay', 's']
 
 def split(phenny, input):
     if casino.game:
-        casino.game.split(input.uid)
+        handid = input.group(2)
+        if not handid:
+            casino.game.split(input.uid)
+
+        if handid.isdigit():
+            casino.game.split(input.uid, handid)
+        else:
+            phenny.write(('NOTICE', input.nick + " Specify the hand using a digit. Ex. !split 1"))
 split.commands = ['split', 'Split', 'sp']
 
 def surrender(phenny, input):
@@ -25,7 +46,14 @@ surrender.commands = ['surrender', 'Surrender']
 
 def doubledown(phenny, input):
     if casino.game:
-        casino.game.doubledown(input.uid)
+        handid = input.group(2)
+        if not handid:
+            casino.game.doubledown(input.uid)
+
+        if handid.isdigit():
+            casino.game.doubledown(input.uid, handid)
+        else:
+            phenny.write(('NOTICE', input.nick + " Specify the hand using a digit. Ex. !doubledown 1"))
 doubledown.commands = ['doubledown','DoubleDown','double','Double']
 
 def hand(phenny, input):
