68574aa463748bd4fe37db514298b4c42594c249
KS-Swifty <32655076+KS-Swifty@users.noreply.github.com>
2017-10-11 04:38:18 +0200

    Fix for the auto-stand of 21 mod.
    It was badly structured, making the next_player function be called twice and causing a double message or a not critical error (if the dealer played after the one getting the 21). This should work better.
    And this time, I configured the tabbing/spacing correctly...

--- casino/blackjack.py
+++ casino/blackjack.py
@@ -198,11 +198,10 @@ class Game:
                 self.phenny.say("BUST! %s went over 21. Their bet was lost to the dealer." % p.players[uid].name)
                 del self.turns[0]
 		
-           if p.players[uid].hand.hand_value() == 21:
+            if p.players[uid].hand.hand_value() == 21:
                 self.phenny.say("Blackjack! %s reached 21, therefore they stand." % p.players[uid].name)
                 self.stand(uid)
-
-            if len(self.turns) > 0 and self.turns[0] == uid: # This players next move
+            elif len(self.turns) > 0 and self.turns[0] == uid: # This players next move
                 self.accept_surrender = False
                 self.accept_doubledown = False
                 self.phenny.write(('NOTICE', p.players[uid].name + " Your Hand: " + str(p.players[uid].hand)))   # NOTICE
