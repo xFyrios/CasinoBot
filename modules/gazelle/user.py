f8c4c0d68b346565222b58797e84bc8d1aa561d6
ZeDoCaixao <zedocaixao@gazellegames.net>
2021-09-06 19:20:38 +0300

    Fixed a crash on !u

--- modules/gazelle/user.py
+++ modules/gazelle/user.py
@@ -8,70 +8,70 @@ Licensed under My Dick.
 import json, string, locale
 locale.setlocale(locale.LC_ALL, 'en_US.utf8')
 
-def user(phenny, input): 
-	""" Gives user stats and information from the site."""
-	etx = '\x03'
-	if not input.group(2):
-		userName = input.nick
-	else:
-		userName = input.group(2).strip()
-	i = phenny.callGazelleApi({'username': userName, 'action': 'userInfo'})
-	print(i)
-	
-	try:
-		# Username
-		o = etx + '10[ ' + etx + '07' + i['username']
-	except KeyError:
-		phenny.say(etx + '10[' + etx + '07 User Not Found ' + etx + '10]')
-		return ""
+def user(phenny, input):
+    """ Gives user stats and information from the site."""
+    etx = '\x03'
+    if not input.group(2):
+        user_name = input.nick
+    else:
+        user_name = input.group(2).strip()
+    i = phenny.callGazelleApi({'username': user_name, 'action': 'userInfo'})
+    print(i)
 
-	# Donor
-	if int(i['enable']) == 1 and int(i['donor']) == 1:
-		o += ' ' + etx + '4<3'
-	elif int(i['donor']) == 1:
-		o += ' ' + etx + '4</3'   
+    try:
+        # Username
+        o = etx + '10[ ' + etx + '07' + i['username']
+    except KeyError:
+        phenny.say(etx + '10[' + etx + '07 User Not Found ' + etx + '10]')
+        return ""
 
-	o += ' ' + etx + '10] :: '
+    # Donor
+    if int(i['enable']) == 1 and int(i['donor']) == 1:
+        o += ' ' + etx + '4<3'
+    elif int(i['donor']) == 1:
+        o += ' ' + etx + '4</3'
 
-	# Class
-	o += '[ ' + etx + '3Class: ' + etx + '07' + i['class'] + ' ' + etx + '10] :: '
+    o += ' ' + etx + '10] :: '
 
-	# Ratio and Stats, depending on paranoia
-	if int(i['paranoia']) < 4:
-		o += '[ ' + etx + '3Up: ' + etx + '07' + i['upload'] + ' ' + etx + '12| ' + etx + '3Down: ' + etx + '07' + i['download'] + ' ' + etx + '12| ' + etx + '3Ratio: ' + etx + '07' + i['ratio'] + ' ' + etx + '10] :: '
-		# Gold
-		o += '[ ' + etx + '3Gold: ' + etx + '07' + i['gold'] + ' ' + etx + '10] :: '
+    # Class
+    o += '[ ' + etx + '3Class: ' + etx + '07' + i['class'] + ' ' + etx + '10] :: '
 
-	# Is the user enabled
-	if int(i['enable']) == 1:
-		o += '[ ' + etx + '3Enabled: ' + etx + '9Yes ' + etx + '10] :: '
-	else:
-		o += '[ ' + etx + '3Enabled: ' + etx + '4No ' + etx + '10] :: '
+    # Ratio and Stats, depending on paranoia
+    if int(i['paranoia']) < 4:
+        o += '[ ' + etx + '3Up: ' + etx + '07' + i['upload'] + ' ' + etx + '12| ' + etx + '3Down: ' + etx + '07' + i['download'] + ' ' + etx + '12| ' + etx + '3Ratio: ' + etx + '07' + i['ratio'] + ' ' + etx + '10] :: '
+        # Gold
+        o += '[ ' + etx + '3Gold: ' + etx + '07' + i['gold'] + ' ' + etx + '10] :: '
 
-	# When was the user last seen
-	if int(i['paranoia']) < 5:
-		o += '[ ' + etx + '3Last Seen: ' + etx + '07' + i['lastseen'] + ' ' + etx + '10] :: '
+    # Is the user enabled
+    if int(i['enable']) == 1:
+        o += '[ ' + etx + '3Enabled: ' + etx + '9Yes ' + etx + '10] :: '
+    else:
+        o += '[ ' + etx + '3Enabled: ' + etx + '4No ' + etx + '10] :: '
 
-	o += etx + '10[ ' + etx + '3Line Count:' + etx + '07 ' + locale.format('%d', int(i['linecount']), 1) + ' ' + etx + '10] :: '
+    # When was the user last seen
+    if int(i['paranoia']) < 5:
+        o += '[ ' + etx + '3Last Seen: ' + etx + '07' + i['lastseen'] + ' ' + etx + '10] :: '
 
-	"""
-	# Build the irc bonus tag
-	o += '[ '  
-	if int(i['bonusdisabled']) == 1:
-		o += etx + '3IRC Bonus: ' + etx + '4No [Disabled]'
-	elif int(i['bonusdisabled']) != 1 and int(i['online']) == 1:
-		o += etx + '3IRC Bonus: ' + etx + '9Yes'
-	elif int(i['online']) == 2:
-		o += etx + '3IRC Bonus: ' + etx + '7No [AFK]'
-	else:
-		o += etx + '3IRC Bonus: ' + etx + '4No [Offline]'
-	o += ' ' + etx + '10] :: '
-	"""
+    o += etx + '10[ ' + etx + '3Line Count:' + etx + '07 ' + locale.format('%d', int(i['linecount']), 1) + ' ' + etx + '10] :: '
+    """
+    # Build the irc bonus tag
+    o += '[ '
+    if int(i['bonusdisabled']) == 1:
+        o += etx + '3IRC Bonus: ' + etx + '4No [Disabled]'
+    elif int(i['bonusdisabled']) != 1 and int(i['online']) == 1:
+        o += etx + '3IRC Bonus: ' + etx + '9Yes'
+    elif int(i['online']) == 2:
+        o += etx + '3IRC Bonus: ' + etx + '7No [AFK]'
+    else:
+        o += etx + '3IRC Bonus: ' + etx + '4No [Offline]'
+    o += ' ' + etx + '10] :: '
+    """
+    o += '[ {0}3Profile: {0}07https://{1}/user.php?id={2} {0}10]'.format(
+        etx,
+        phenny.config.gazelle_url,
+        i['userid'])
 
-
-	o += '[ ' + etx + '3Profile: ' + etx + '07https://' + phenny.config.gazelle_url + '/user.php?id=' + i['userid'] + ' ' + etx + '10]'
-
-	phenny.say(o)
+    phenny.say(o)
 
 user.commands = ['u', 'user']
 user.priority = 'high'
@@ -82,7 +82,7 @@ def tip(phenny, input):
 
     args = input.group(0).split()
     args.pop(0)
-    
+
     totip = args[0]
     amount = args[1]
 
@@ -91,13 +91,17 @@ def tip(phenny, input):
     elif int(amount) <= 0:
         phenny.say('You tried to tip an invalid amount.')
     else:
-        i = phenny.callGazelleApi({'userid': input.uid, 'mod': input.mod, 'totip': totip, 'amount': amount, 'action': 'tipUser'})
-        if i == False or i['status'] == "error":
-            phenny.say(i['error']);
+        i = phenny.callGazelleApi({'userid': input.uid,
+                                   'mod': input.mod,
+                                   'totip': totip,
+                                   'amount': amount,
+                                   'action': 'tipUser'})
+        if not i or i['status'] == "error":
+            phenny.say(i['error'])
         else:
             phenny.say(i['msg'])
 tip.commands = ['tip']
 tip.example = '!tip Daedy 10'
 
-if __name__ == '__main__': 
-	print(__doc__)
+if __name__ == '__main__':
+    print(__doc__)
